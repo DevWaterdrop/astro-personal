dedd84b8bc860877b43bd950d2e58bd89b49d353
DevWaterdrop <dev.waterdrop@gmail.com>
2023-11-18 22:59:54 +0100

    Feat add demo section & fix age

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       screens: {
-        xs: "380px",
+        xs: '380px',
       },
       fontSize: {
-        ["4.5xl"]: ["2.5rem", "1"],
+        ['4.5xl']: ['2.5rem', '1'],
       },
       colors: {
-        blue: "#5fbff9",
-        ["blue--dark"]: "#088cdd",
-        black: "#252525",
-        white: "#ffffff",
-        footer: "#292f36",
-        beige: "#f7f3f0",
+        blue: '#5fbff9',
+        ['blue--dark']: '#088cdd',
+        black: '#252525',
+        white: '#ffffff',
+        footer: '#1c1917',
+        beige: '#f7f3f0',
       },
     },
   },
